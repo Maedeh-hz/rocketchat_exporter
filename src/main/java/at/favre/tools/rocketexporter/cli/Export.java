c809b4fa845a2468ecf8d70ae300d380575abb76
Maede-hz <maedehosseinzade84@gmail.com>
2024-10-14 11:34:10 +0330

    version 3.

--- src/main/java/at/favre/tools/rocketexporter/cli/Export.java
+++ src/main/java/at/favre/tools/rocketexporter/cli/Export.java
@@ -9,12 +9,19 @@ import at.favre.tools.rocketexporter.dto.Conversation;
 import at.favre.tools.rocketexporter.dto.LoginDto;
 import at.favre.tools.rocketexporter.dto.TokenDto;
 import at.favre.tools.rocketexporter.model.Message;
+import lombok.SneakyThrows;
 import picocli.CommandLine;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.PrintStream;
-import java.net.URL;
-import java.util.*;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @CommandLine.Command(description = "Exports rocket chat messages from a specific group/channel.",
@@ -24,13 +31,14 @@ class Export implements Runnable {
     @CommandLine.Option(names = {"-o", "--outFile"}, description = "The file or directory to write the export data to. Will write to current directory with auto generated filename if this arg is omitted. If you want to export multiple conversations you must pass a directory not a file.")
     private File file;
 
-    @CommandLine.Option(names = {"-t", "--host"}, required = true, description = "The rocket chat server. E.g. 'https://myserver.com'")
-    private URL host;
-
+    @CommandLine.Option(names = {"-t", "--host"}, required = false, description = "The rocket chat server. E.g. 'https://myserver.com'")
+    private String host;
+    @CommandLine.Option(names = {"-c", "--config"}, required = true, description = "The rocket chat exporter config file. E.g. './config.cnf'")
+    private File configFile;
     @CommandLine.Option(names = {"-u", "--user"}, required = false, description = "RocketChat username for authentication.")
     private String username;
 
-    @CommandLine.Option(names = {"-k", "--user-id"}, required = true, description = "RocketChat Personal Access Token user ID.")
+    @CommandLine.Option(names = {"-k", "--user-id"}, required = false, description = "RocketChat Personal Access Token user ID.")
     private String userId;
 
     @CommandLine.Option(names = {"--debug"}, description = "Add debug log output to STDOUT.")
@@ -40,33 +48,37 @@ class Export implements Runnable {
     private int maxMessages = 50000;
 
     public static void main(String[] args) {
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+        System.out.println("Text in UTF-8");
         int exitCode = new CommandLine(new Export())
                 .setCaseInsensitiveEnumValuesAllowed(true).execute(args);
         System.exit(exitCode);
     }
 
+    @SneakyThrows
     @Override
     public void run() {
         PrintStream out = System.out;
 
+        FileReader fileReader = new FileReader(configFile);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        host = bufferedReader.readLine();
+        userId = bufferedReader.readLine();
+        String password = bufferedReader.readLine();
+        file = new File(bufferedReader.readLine());
+
+        System.out.println("host = " + host);
+        System.out.println("userId = " + userId);
+        System.out.println("password or token = " + password);
+        System.out.println("store dir = " + file.getAbsolutePath());
         if (username == null && userId == null) {
             out.println("You have to use a username or a token user ID to continue.");
             System.exit(-1);
         }
-
-        out.println("Please enter your RocketChat password or token:");
-
-        String password;
-        if (System.console() != null) {
-            password = String.valueOf(System.console().readPassword());
-        } else {
-            password = new Scanner(System.in).next();
-        }
-
         try {
             RocketExporter exporter = RocketExporter.newInstance(
                     Config.builder()
-                            .host(host.toURI())
+                            .host(URI.create(host))
                             .httpDebugOutput(debug)
                             .build());
 
@@ -139,7 +151,10 @@ class Export implements Runnable {
                 final ExportFormat format = new SlackCsvFormat();
                 final int offset = 0;
                 final int maxMsg = maxMessages;
-                final File outFile = generateOutputFile(file, selectedGroup.getName(), type, format);
+
+                String directoryName = selectedGroup.getName().replaceAll("[^a-zA-Z0-9]", "-");
+
+                final File outFile = generateOutputFile(file, directoryName, type, format);
 
                 try {
                     switch (type) {
@@ -181,14 +196,5 @@ class Export implements Runnable {
             out.mkdirs();
         }
         return out;
-//        if (provided.isDirectory()) {
-//            String filename = type.name.replaceAll(" ", "-") + "_" + contextName + "_" + DateTimeFormatter
-//                    .ofPattern("yyyyMMddHHmmss")
-//                    .withZone(ZoneId.of("UTC"))
-//                    .format(Instant.now()) + "." + format.fileExtension();
-//            new File(provided, contextName);
-//        } else {
-//            return provided;
-//        }
     }
 }
